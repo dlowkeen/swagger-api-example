4fb27f213d0bec1130588718bba40c9e2ea71014
Donovan <donovan@Donovan-MacBook-Pro.local>
2019-04-22 08:32:46 -0700

    added swagger comments to update documentation

--- cookies/index.ts
+++ cookies/index.ts
@@ -3,8 +3,57 @@ import * as cookies from './cookies';
 
 let router = express.Router();
 
+/**
+ * @swagger
+ * /cookies:
+ *    post:
+ *      tags:
+ *          - Cookies
+ *      summary: This should create a new cookie.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              flavor:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id of recently added cookie.
+ */
 router.route('').post(cookies.addOne);
+
+/**
+ * @swagger
+ * /ice-cream:
+ *    get:
+ *      tags:
+ *          - Ice Cream
+ *      summary: This should return all cookie flavors.
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id of all cookie flavors.
+ */
 router.route('').get(cookies.getAll);
+
+/**
+ * @swagger
+ * /cookie/:flavorId:
+ *    get:
+ *      tags:
+ *          - Cookie
+ *      summary: This should return the flavor and id of the specified cookie
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id of speciifed cookie flavors.
+ */
 router.route('/:flavorId').get(cookies.getOne);
 
 export = router;
--- ice-cream/index.ts
+++ ice-cream/index.ts
@@ -3,8 +3,57 @@ import * as iceCream from './ice-cream';
 
 let router = express.Router();
 
+/**
+ * @swagger
+ * /ice-cream:
+ *    post:
+ *      tags:
+ *          - Ice Cream
+ *      summary: This should create a new ice cream.
+ *      consumes:
+ *        - application/json
+ *      parameters:
+ *        - name: body
+ *          in: body
+ *          schema:
+ *            type: object
+ *            properties:
+ *              flavor:
+ *                type: string
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id.
+ */
 router.route('').post(iceCream.addOne);
+
+/**
+ * @swagger
+ * /ice-cream:
+ *    get:
+ *      tags:
+ *          - Ice Cream
+ *      summary: This should return all cookie flavors.
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id of all cookie flavors.
+ */
 router.route('').get(iceCream.getAll);
+
+/**
+ * @swagger
+ * /ice-cream/:flavorId:
+ *    get:
+ *      tags:
+ *          - Ice Cream
+ *      summary: This should return the flavor and id of the specified ice cream
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id of speciifed ice cream flavors.
+ */
 router.route('/:flavorId').get(iceCream.getOne);
 
 export = router;
